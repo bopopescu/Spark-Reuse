63f9633ededdf20fcf040b67e2627f8f4432505b
Noany <zengdan@ict.ac.cn>
2015-06-11 16:21:04 +0800

    initialize

--- build_maven.sh
+++
@@ -1,6 +0,0 @@
-#MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m" mvn -Dhadoop.version=2.2.0 -Phive -Phive-0.13.1 -Phive-thriftserver -DskipTests -Dscalastyle.failOnViolation=false clean package install
-#mvn -Dhadoop.version=2.2.0 -Phive -Phive-0.13.1 -Phive-thriftserver -DskipTests install
-#sbt/sbt -Dhadoop.version=2.2.0 -Phive -Phive-0.13.1 -Phive-thriftserver gen-idea
-./make-distribution.sh --tgz -Dhadoop.version=2.2.0 -Phive -Phive-0.13.1 -Phive-thriftserver
-#cd ~/sqltest
-#sbt package
--- sql/core/src/main/scala/org/apache/spark/sql/execution/cp.sh
+++
@@ -1,1 +0,0 @@
-cp $1 /Users/zengdan/spark-1.2.0/sql/core/src/main/scala/org/apache/spark/sql/execution/$1
